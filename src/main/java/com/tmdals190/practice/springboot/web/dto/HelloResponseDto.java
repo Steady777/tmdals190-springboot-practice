7eb79fb9ca96502d205f3225e06a2b71ee7da5e8
tmdals190 <52847494+tmdals190@users.noreply.github.com>
2020-11-01 15:15:17 +0900

    롬복 기능 테스트 추가

--- src/main/java/com/tmdals190/practice/springboot/web/dto/HelloResponseDto.java
+++ src/main/java/com/tmdals190/practice/springboot/web/dto/HelloResponseDto.java
@@ -1,4 +1,13 @@
 package com.tmdals190.practice.springboot.web.dto;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
 }
